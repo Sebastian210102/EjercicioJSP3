aa9f1b14ebca3abda1836f062d2c28d7d3e6a72c
Sebastian210102 <72526765+Sebastian210102@users.noreply.github.com>
2024-02-16 20:13:12 -0600

    Resolución

---
+++ app.js
@@ -0,0 +1,12 @@
+//Declaramos las variables
+let contador=0;
+let numero=1;
+
+while (contador<10){
+    if(numero %2===0){
+        console.log("El numero par es " + numero);
+        contador++;        
+    }
+        numero++;
+    
+}
